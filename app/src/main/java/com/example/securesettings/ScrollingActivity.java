89fe132a9df5d0937b24f27e56221dc71370ab5a
Aniruddha - Kali PC <aniruddha2008@gmail.com>
2019-12-06 13:09:56 +0530

    removed demo snackbar

--- app/src/main/java/com/example/securesettings/ScrollingActivity.java
+++ app/src/main/java/com/example/securesettings/ScrollingActivity.java
@@ -36,8 +36,6 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
 
                 Intent shareIntent = new Intent();
                 shareIntent.setAction(Intent.ACTION_SEND);
